24ce7a7405542192512917a3f5d50c376fbd0b62
cyrilsojangit <61079720+cyrilsojangit@users.noreply.github.com>
2021-05-06 11:16:19 +0530

    first commit

---
+++ src/main/java/com/dev/accountmanager/api/AccountManagerApplication.java
@@ -0,0 +1,15 @@
+package com.dev.accountmanager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AccountManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagerApplication.class, args);
+	}
+
+}
